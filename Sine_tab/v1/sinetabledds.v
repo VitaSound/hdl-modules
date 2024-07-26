b26f635aeca1a22872522195764c8224af39f924
Lena-2023 <alenataylor@yandex.ru>
2024-07-26 21:34:19 +0300

    26-07-24

--- Sine_tab/v1/sinetabledds.v
+++ Sine_tab/v1/sinetabledds.v
@@ -1,4 +1,3 @@
-
 module sinetabledds(
 		
 	input wire CLK,
@@ -18,17 +17,19 @@ module sinetabledds(
 
 
 	function [N-1:0] sin4;
-		input [N:0] t4;     // t4 
+		input [2:0] t4;
+		t4 = DDS[N-2:N-4];     // t4 
+		
 		begin
-			case (t4)
-				 8'd00: sin4 = 0.0000;    //sin(0.0000) = 0.0000
-                 8'd01: sin4 = 0.1710;    //sin(0.1718) = 0.1710
-                 8'd02: sin4 = 0.3599;    //sin(0.3682) = 0.3599
-                 8'd03: sin4 = 0.5350;    //sin(0.5645) = 0.5350
-                 8'd04: sin4 = 0.6895;    //sin(0.7609) = 0.6895
-                 8'd05: sin4 = 0.8176;    //sin(0.9572) = 0.8176
-                 8'd06: sin4 = 0.9142;    //sin(1.1536) = 0.9142
-                 8'd07: sin4 = 0.9757;    //sin(1.3499) = 0.9757
+			case (t4) //t4
+				 3'b000: sin4 = 0.0000;    //sin(0.0000) = 0.0000
+                 3'b001: sin4 = 0.1710;    //sin(0.1718) = 0.1710
+                 3'b010: sin4 = 0.3599;    //sin(0.3682) = 0.3599
+                 3'b011: sin4 = 0.5350;    //sin(0.5645) = 0.5350
+                 3'b100: sin4 = 0.6895;    //sin(0.7609) = 0.6895
+                 3'b101: sin4 = 0.8176;    //sin(0.9572) = 0.8176
+                 3'b110: sin4 = 0.9142;    //sin(1.1536) = 0.9142
+                 3'b111: sin4 = 0.9757;    //sin(1.3499) = 0.9757
 
 				default: sin4 = {N{1'b0}};
 			endcase
@@ -39,8 +40,8 @@ module sinetabledds(
     
 	always @ (posedge CLK)
 		begin
-			//accumulator <= accumulator + 1'b1;
-			case (quadrant)
+			
+			case (DDS[N:N-1])
 		
 				2'b00: DDSout_sine  <= {1'b1, sin4(t_even)};
 				2'b01: DDSout_sine  <= {1'b1, sin4(t_odd)};
